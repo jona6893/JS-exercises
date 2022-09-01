aa39771f3d2e5e061e454fd19a5e9d1c120ee109
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-09-01 08:52:14 +0200

    another methode

--- countingArray/script.js
+++ countingArray/script.js
@@ -27,3 +27,18 @@ function myLoop() {
     /* console.log(i) */
 }
 }
+
+
+//smarter solution
+
+/* const arr = [];
+let counter = 0;
+init();
+
+function init() {
+  //
+  counter += 1;
+  arr.unshift(counter);
+  console.log(arr.slice(0, 9));
+  setTimeout(init, 500);
+} */
